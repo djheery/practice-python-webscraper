01623a0587cd9dfa611064ca9c44db042ebc8415
Daniel Heery <danieljheery@gmail.com>
2021-09-22 14:20:32 +0000

    Start of a webscraper

---
+++ web-scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+
+job_elements = results.find_all("div", class_="card-content")
+for job_elem in job_elements:
+    title_element = job_elem.find("h2", class_="title")
+    company_element = job_elem.find("h3", class_="company")
+    location_element = job_elem.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip(), end="\n"*2)
+    

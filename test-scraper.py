148e945a322230dbebebc381f3ebff6ed084d98d
Daniel <danieljheery@gmail.com>
2021-09-23 17:11:04 +0100

    Got name & website

---
+++ test-scraper.py
@@ -0,0 +1,57 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+s = HTMLSession()
+base_url = 'https://spotlight.com'
+url = 'https://www.spotlight.com/contacts/listing/search?Category=Contacts%5CDrama%20%26%20Dance%5CChoreographers'
+
+def get_data(url):
+  r = s.get(url)
+  soup = BeautifulSoup(r.content, 'html.parser')
+  return soup
+  
+def get_about_info(about):
+  text = about
+  return 'Some about content to fill in what is missing'
+
+def unscramble_email(email):
+  return 'email@email.com'
+
+def get_targeted_content(page):
+  name = page.find('span', {'data-rc': 'listing-Name'})
+  about = page.find('span', {'data-rc': 'listing-About'})
+  email = page.find('a', {'data-rc': 'listing-body-Email address'})
+  website = page.find('a', {'data-rc': 'listing-body-Website'})
+
+  return {
+    'name': name.text.strip(),
+    'website': website.get('href'),
+    'email': unscramble_email(email),
+    'about': get_about_info(about)
+  }
+
+def get_links(s): 
+  linkArr = []
+  links = s.find_all('a', {'class': 'searchResultViewButton'})
+  for link in links:
+    linkArr.append(link.get('href'))
+  
+  return  linkArr
+
+def get_scraping(links):
+  data = []
+  for link in links:
+    page = get_data(f'{base_url}{link}')
+    target_content = get_targeted_content(page)
+    data.append(target_content)
+  return data
+
+
+soup = get_data(url)
+links = get_links(soup)
+content = get_scraping(links)
+
+pprint(content)
+
+  
--- web-scraper.py
+++ web-scraper.py
@@ -1,17 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 PATH = "/Users/user/documents/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 URL = "https://www.spotlight.com/contacts/listing/search?Category=Contacts%5CDrama%20%26%20Dance%5CChoreographers"
+
+base_url = 'https://www.spotlight.com'
+
 driver.get(URL)
-
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find_all("li", class_="searchResultItem")
+button = soup.find_all("a", class_="searchResultViewButton")
 
-for result in results:
-  button = result.find("a", class_="searchResultViewButton")
-  print(button)
+links = []
+
+def scrapeMe(l): 
+  driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
+  driver.get(f"{base_url}{l}")
+  soup_two = BeautifulSoup(page.content, "html.parser")
+  about = soup_two.find(attrs={"data-rc":"listing-About"})
+  print(driver.geCurrentUrl())
+
+
+
+for b in button:
+  links.append(b.get('href'))
+
+for link in links: 
+  scrapeMe(link)

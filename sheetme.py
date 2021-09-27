92325eab95862e69925ef822c7e579069529b133
Daniel <danieljheery@gmail.com>
2021-09-27 18:43:49 +0100

    practicing openpyxl

--- sheetme.py
+++ sheetme.py
@@ -5,6 +5,40 @@ import datetime
 
 wb = Workbook()
 ws = wb.active
+# Assigns the Title of Hello World to the active worksheet
 ws.title = 'Hello World'
+# Looks for worksheet with the name Hello World
+ws1 = wb['Hello World']
+# Create Sheet - Appends to the List by Default
+ws2 = wb.create_sheet('Python is Cool')
 
-print(ws.title)
+# Modifying a Cell - Access by c.value
+c = ws['A4']
+ws['A4'] = 4
+
+# Accesing a range of cells
+cell_range = ws2['A1':'D2']
+# Access Collum
+column_c = ws['A']
+column_range = ws['C':'D']
+row_ten = ws[10]
+row_range = ws[5:10]
+
+# Iterate through the Rows
+for row in ws.iter_rows(min_row=1, max_col=3, max_row=4):
+  for cell in row:
+    pprint(cell)
+# for every 'row' definded by iter_rows(minimum row= row-1, how many columns= 10, where do you want )
+# Prints each cell in a row - A1, B1, C1 => A2, B2, C2
+
+# Iterate through the Columns
+for col in ws.iter_cols(min_row=1, max_col=3, max_row=4):
+  for cell in col:
+    print(cell)
+# Prints each cell in a column A1, A2, A3 - B1, B2, B3, C1 C2 C3
+
+# pprint(tuple(ws.rows))
+
+
+
+# pprint(row_range)
